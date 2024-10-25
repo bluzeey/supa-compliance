ab3d7283d29b9fafc698a61a28f34d419171c73b
Sahil Maheshwari <sahilm1711@gmail.com>
2024-10-25 14:16:29 +0530

    Adding changes for supabase

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 
 const ProfilePage = async () => {
-  const supabase = createClient();
+  const supabase = await createClient();
 
   const {
     data: { user },
